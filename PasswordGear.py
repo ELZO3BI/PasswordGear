445f6a2baf6b31fe0c7a3a0a1d82e288e27a954f
ELZO <zoubinono92@gmail.com>
2019-03-11 13:39:56 +0200

    Add files via upload
    PasswordGear.py Updated-V2.

--- PasswordGear.py
+++ PasswordGear.py
@@ -1,32 +1,38 @@
-# Python36
+# Version += Python3
 
 import random
 import string
 import hashlib
+import sys
 
 gen1 = string.digits + string.ascii_lowercase + string.punctuation  # Variable: gen1, contains numbers and lowercase alphabet and punctuation marks.
 gen2 = string.digits                                                # Variable: gen2, contains only numbers.
 gen3 = string.ascii_lowercase + string.ascii_uppercase              # Variable: gen3, contains only lowercase and uppercase alphabet.
 gen4 = string.punctuation                                           # Variable: gen4, contains only punctuation marks.
 
-print('Random Password Generating Or Hashing.\n')
 
-print ('1. Regular Password, length "10".')
-print ('2. Medium Password, length "15".')
-print ('3. Strong Password, length "25".')
-print ('4. Custom Password, length "Custom".')
-print ('5. Only Alphabet, Lower & Upper case Password, length "25".')
-print ('6. Only Punctuation Marks Password, length "20".')
-print ('7. Only Numbers Password, length "Custom".')
-print ('---------------------------------------------------------------------')
-print ('8. Encrypt Your Password With Couple Of Hashing Methods/Algorithms.\n')
+def main():
+    print('Random Password Generating Or Hashing.\n')
+
+    print ('1. Regular Password, length "10".')
+    print ('2. Medium Password, length "15".')
+    print ('3. Strong Password, length "25".')
+    print ('4. Custom Password, length "Custom".')
+    print ('5. Only Alphabet, Lower & Upper case Password, length "25".')
+    print ('6. Only Punctuation Marks Password, length "20".')
+    print ('7. Only Numbers Password, length "Custom".')
+    print ('---------------------------------------------------------------------')
+    print ('8. Encrypt Your Password With Couple Of Hashing Methods/Algorithms.\n')
+
+main()
 
 que = int(input('Select The Type Of Password You Want, Or To Enrypt [1-8]: '))  # creating our input section. either you want to choose password outputs or encrypting.
 print('')             # ignore this it's just for skipping lines.
-choice = int(que)     # choice = our input section
+choice = (que)     # choice = our input section
 
 if choice == 1:
     print(''.join(random.sample(gen1 * 10, 10)))  # prints a random password length is 10, mixed numbers with alphabet and punctuation marks.
+    
 
 elif choice == 2:
     print(''.join(random.sample(gen1 * 15, 15)))  # prints a random password length is 15, mixed numbers with alphabet and punctuation marks.
@@ -64,49 +70,121 @@ elif choice == 8:
     print('')
 
     if otherchoice == 1:
-        tohash = input('Write your password to encrypt it with MD5: ')
-        hasher = hashlib.md5()
-        hasher.update(str(tohash).encode('utf-8'))  # ecnrypts your password with md5.
-        hashed_password = hasher.hexdigest()
-        print('\n[-] Your Hashed Password Is:', hashed_password) # prints your encrypted password.
+        def encrypter():
+            tohash = input('Write your password to encrypt it with MD5: ')
+            hasher = hashlib.md5()
+            hasher.update(str(tohash).encode('utf-8'))  # ecnrypts your password with md5.
+            hashed_password = hasher.hexdigest()
+            print('\n[+] Your Encrypted Password Is:', hashed_password)  # prints your encrypted password.
+            print('-------------------------------------------------------------')
+            more = input('\nDo you want to enrypt more passwords [Y/n]?: ')
+            print('')
+            if more == 'Y' or more == 'y':
+                return encrypter()
+            elif more == 'N' or more == 'n':
+                print('[!] Exiting.')
+                sys.exit()
+            else:
+                print('[-] Wrong Input.')
 
     elif otherchoice == 2:
-        tohash = input('Write your password to encrypt it with SHA-1 : ')
-        hasher = hashlib.sha1()
-        hasher.update(str(tohash).encode('utf-8'))  # ecrypts your password with sha-1, secure hash algorithm 1.
-        hashed_password = hasher.hexdigest()
-        print('\n[-] Your Hashed Password Is:', hashed_password) # prints your encrypted password.
+        def encrypter():
+            tohash = input('Write your password to encrypt it with SHA-1 : ')
+            hasher = hashlib.sha1()
+            hasher.update(str(tohash).encode('utf-8'))  # ecrypts your password with sha-1, secure hash algorithm 1.
+            hashed_password = hasher.hexdigest()
+            print('\n[+] Your Encrypted Password Is:', hashed_password)  # prints your encrypted password.
+            print('-----------------------------------------------------------------------')
+            more = input('\nDo you want to encrypt more passwords [Y/n]?: ')
+            print('')
+            if more == 'Y' or more == 'y':
+                return encrypter()
+            elif more == 'N' or more == 'n':
+                print('[!] Exiting.')
+                sys.exit()
+            else:
+                print('[-] Wrong input.')
 
     elif otherchoice == 3:
-        tohash = input('Write your password to encrypt it with SHA-224: ')
-        hasher = hashlib.sha224()
-        hasher.update(str(tohash).encode('utf-8'))  # ecrypts your password with sha-224, secure hash algorithm 224.
-        hashed_password = hasher.hexdigest()
-        print('\n[-] Your Hashed Password Is:', hashed_password) # prints your encrypted password.
+        def encrypter():
+            tohash = input('Write your password to encrypt it with SHA-224: ')
+            hasher = hashlib.sha224()
+            hasher.update(str(tohash).encode('utf-8'))  # ecrypts your password with sha-224, secure hash algorithm 224.
+            hashed_password = hasher.hexdigest()
+            print('\n[+] Your Encrypted Password Is:', hashed_password)  # prints your encrypted password.
+            print('---------------------------------------------------------------------------------')
+            more = input('\nDo you want to encrypt more passwords [Y/n]?: ')
+            print('')
+            if more == 'Y' or more == 'y':
+                return encrypter()
+            elif more == 'N' or more == 'n':
+                print('[!] Exiting.')
+                sys.exit()
+            else:
+                print('[-] Wrong input.')
 
     elif otherchoice == 4:
-        tohash = input('Write your password to encrypt it with SHA-256: ')
-        hasher = hashlib.sha256()
-        hasher.update(str(tohash).encode('utf-8'))  # encrypts your password with sha-256, secure hash algorithm 256.
-        hashed_password = hasher.hexdigest()
-        print('\n[-] Your Hashed Password Is:', hashed_password) # prints your encrypted password.
+        def encrypter():
+            tohash = input('Write your password to encrypt it with SHA-256: ')
+            hasher = hashlib.sha256()
+            hasher.update(str(tohash).encode('utf-8'))  # encrypts your password with sha-256, secure hash algorithm 256.
+            hashed_password = hasher.hexdigest()
+            print('\n[+] Your Encrypted Password Is:', hashed_password)  # prints your encrypted password.
+            print('---------------------------------------------------------------------------------------------------')
+            more = input('\nDo you want to encrypt more passwords [Y/n]?: ')
+            print('')
+            if more == 'Y' or more == 'y':
+                return encrypter()
+            elif more == 'N' or more == 'n':
+                print('[!] Exiting.')
+                sys.exit()
+            else:
+                print('[-] Wrong Input.')
 
     elif otherchoice == 5:
-        tohash = input('Write your password to encrypt it with SHA-384: ')
-        hasher = hashlib.sha384()
-        hasher.update(str(tohash).encode('utf-8'))  # encrypts your password with sha-384, secure hash algorithm 384.
-        hashed_password = hasher.hexdigest()
-        print('\n[-] Your Hashed Password Is:', hashed_password) # prints your encrypted password.
+        def encrypter():
+            tohash = input('Write your password to encrypt it with SHA-384: ')
+            hasher = hashlib.sha384()
+            hasher.update(str(tohash).encode('utf-8'))  # encrypts your password with sha-384, secure hash algorithm 384.
+            hashed_password = hasher.hexdigest()
+            print('\n[+] Your Encrypted Password Is:', hashed_password)  # prints your encrypted password.
+            print('-----------------------------------------------------------------------------------------------------------------------------')
+            more = input('\nDo you want to encrypt more passwords [Y/n]?: ')
+            print('')
+            if more == 'Y' or more == 'y':
+                return encrypter()
+            elif more == 'N' or more == 'n':
+                print('[!] Exiting.')
+                sys.exit()
+            else:
+                print('[-] Wrong input.')
 
     elif otherchoice == 6:
-        tohash = input('Write your password to encrypt it with SHA-512: ')
-        hasher = hashlib.sha512()
-        hasher.update(str(tohash).encode('utf-8'))  # encrypts your password with sha-512, secure hash algorithm 512.
-        hashed_password = hasher.hexdigest()
-        print('\n[-] Your Hashed Password Is:', hashed_password) # prints your encrypted password.
+        def encrypter():
+            tohash = input('Write your password to encrypt it with SHA-512: ')
+            hasher = hashlib.sha512()
+            hasher.update(str(tohash).encode('utf-8'))  # encrypts your password with sha-512, secure hash algorithm 512.
+            hashed_password = hasher.hexdigest()
+            print('\n[+] Your Encrypted Password Is:', hashed_password)  # prints your encrypted password.
+            print('----------------------------------------------------------------------------------------------------------------------------------------------------------------')
+            more = input('\nDo you want to encrypt more passwords [Y/n]?: ')
+            print('')
+            if more == 'Y' or more == 'y':
+                return encrypter()
+            elif more == 'N' or more == 'n':
+                print('[!] Exiting.')
+                sys.exit()
+            else:
+                print('[-] Wrong input.')
 
     else:
-        print('[-] You must choose any of the displayed hash types [1-6]')
+        print('[-] You must choose any of the displayed encryption types [1-6]')
 
 else:
     print('[-] You have only 8 selections [1-8]')
+
+try:
+    if __name__ == '__main__':
+        encrypter()
+except:
+    pass
